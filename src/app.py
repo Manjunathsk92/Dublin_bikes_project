5e948d318ecbdcf1c599edf7cf64b858678bf723
Anjali <anjali.pgowda@.com>
2018-04-17 22:15:31 +0100

    Integrated trends and the occupancy details

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from main import Main
 from flask import g
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 import mysql.connector
 app = Flask(__name__)
 
@@ -66,6 +66,28 @@ def station_details():
     print("#found {} stations", len(rows))
     return jsonify(stations=[dict(row.items()) for row in rows])
  
+
+@app.route('/charts_daily', methods=['GET', 'POST'])
+def get_charts_daily():
+    """Gets the average number of bikes and stands for each day"""
+    station_number = request.args.get('station_number')
+    type = request.args.get('type')
+    return daily_avg_dynamic(station_number, type)
+
+def daily_avg_dynamic(station_number, type='daily'):
+    """Returns the average number of bike per day"""
+    st_num = int(station_number)
+    engine = get_db('se-project-db.cuph6akhej5q.us-east-2.rds.amazonaws.com', '3306', 'projectdb', 'Manjunathsk92',
+                    'Manjunathsk92')
+    if type == 'daily':
+        sql = text("SELECT AVG(available_bikes) as avg_bikes, AVG(available_bike_stands) as avg_stands, station_number, DATE_FORMAT(FROM_UNIXTIME(`insert_timestamp`), '%e %b %Y') AS 'date_formatted' FROM Dublin_bikes_realtime_week_data WHERE insert_timestamp >= unix_timestamp(curdate() - INTERVAL DAYOFWEEK(curdate())+6 DAY) AND insert_timestamp < unix_timestamp(curdate() - INTERVAL DAYOFWEEK(curdate())-1 DAY) and station_number = " + str(st_num) +" group by date_formatted order by insert_timestamp asc;")
+    else:
+        sql = text("SELECT AVG( available_bikes ) as avg_bikes, AVG(available_bike_stands) as avg_stands, from_unixtime(insert_timestamp) AS 'date_formatted' FROM Dublin_bikes_realtime_week_data where insert_timestamp < unix_timestamp(curdate() - 1) and insert_timestamp > unix_timestamp(curdate() - 2) and station_number = "+ str(st_num) +" GROUP BY DATE( date_formatted ), HOUR( date_formatted );")
+    results = engine.execute(sql).fetchall()
+    engine.dispose()
+    print('#found {} stations', len(results))
+    return jsonify(stations=[dict(row.items()) for row in results])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
