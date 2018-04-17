0c4ee48e0e716dd895b1104c46f6e4fabe3764c3
TejaswiSuryas <tejaswi.suryas@gmail.com>
2018-04-17 17:33:24 +0100

    adding modified files

--- dublibikes/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from dublinbikes import dublinbikes
-
-@app.route('/')
-def index():
-    returnDict={}
-    returnDict['user']='Anjali'
-    return render_template("index.html",**returnDict)
